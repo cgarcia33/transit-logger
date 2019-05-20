959ea9bd86b69f0f3eb88de926124cf5b160b00f
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-05-19 23:47:25 -0500

    deploy to firebase

--- src/Breakdown.js
+++ src/Breakdown.js
@@ -8,6 +8,20 @@ class Breakdown extends Component {
     return (
       <div className="Breakdown">
         <h1>Breakdown</h1>
+        <div className="stats">
+          <h2>Total Time Spent Commuting</h2>
+          <div className="stat">50 hours</div>
+          <h2>Average Commute</h2>
+          <div className="stat">1 hour 50 minutes</div>
+          <h2>Longest Commute</h2>
+          <div className="stat">2 hours 45 minutes</div>
+          <h2>Shortest Commute</h2>
+          <div className="stat">1 hour 10 minutes</div>
+          <h2>Most Used Transit Line</h2>
+          <div className="stat"> Purple Line</div>
+          <h2>Least Used Transit Line</h2>
+          <div className="stat">Pace 290 Touhy</div>
+        </div>
       </div>
     );
   }
