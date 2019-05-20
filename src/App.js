b102186548419f6f647d2626d25ae117ebaa3f75
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-05-19 23:06:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { lines: [] };
+
+  componentDidMount() {
+    import("./static/combinedLines.json")
+      .then(json => {
+        this.setState({ lines: json.lines });
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Transit Logger</h1>
+        {this.state.lines.map((line, i) => (
+          <div
+            className="line-box"
+            key={i}
+            style={{ backgroundColor: line.color }}
+          >
+            <div
+              className="line-pic"
+              style={{
+                backgroundImage: `url(${require(`./static/images/${
+                  line.id
+                }.jpg`)})`
+              }}
+            />
+            <div className="line-name">{line.name}</div>
+          </div>
+        ))}
+        <div className="links">
+          <Link to="/breakdown">Breakdown</Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Breakdown.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./breakdown.css";
+
+class Breakdown extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="Breakdown">
+        <h1>Breakdown</h1>
+      </div>
+    );
+  }
+}
+
+export default Breakdown;
