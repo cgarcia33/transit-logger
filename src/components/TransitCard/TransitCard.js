b541d0b5e8ecf44549623e0db1d16b4aa4dafa32
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-05-25 16:26:06 -0500

    toggle buttons on TransitCard click

--- src/components/TransitCard/TransitCard.js
+++ src/components/TransitCard/TransitCard.js
@@ -2,21 +2,41 @@ import React, { Component } from "react";
 import "./TransitCard.css";
 
 class TransitCard extends Component {
-  state = {};
+  state = { showButtons: false };
+
+  handleClick = () => {
+    this.setState({ showButtons: !this.state.showButtons });
+  };
+
+  renderButtons = () => {
+    return this.state.showButtons ? (
+      <div className="btn-row">
+        <button className="btn">Start Trip</button>
+        <button className="btn">Breakdown</button>
+      </div>
+    ) : null;
+  };
 
   render() {
     const line = this.props.transitLine;
     return (
-      <div className="line-box" style={{ backgroundColor: line.color }}>
+      <div>
         <div
-          className="line-pic"
-          style={{
-            backgroundImage: `url(${require(`../../static/images/${
-              line.id
-            }.jpg`)})`
-          }}
-        />
-        <div className="line-name">{line.name}</div>
+          className="line-box"
+          style={{ backgroundColor: line.color }}
+          onClick={this.handleClick}
+        >
+          <div
+            className="line-pic"
+            style={{
+              backgroundImage: `url(${require(`../../static/images/${
+                line.id
+              }.jpg`)})`
+            }}
+          />
+          <div className="line-name">{line.name}</div>
+        </div>
+        {this.renderButtons()}
       </div>
     );
   }
