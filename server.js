3ab28e6872d0473c2b515c097a007115d26c8385
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-05-26 23:16:31 -0500

    add cors middleware

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
