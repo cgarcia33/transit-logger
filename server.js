9d36ebbfd56356367b50779b9b98b495640f864b
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-05-26 22:00:28 -0500

    write routes for getting/patching line status

--- server.js
+++ server.js
@@ -10,11 +10,32 @@ app.use(bodyParser.json());
 // DB Config
 const db = require("./keys").mongoURI;
 
+// Models
+const LineStatus = require("./models/Status");
+
 // Connect to Mongo
 mongoose
   .connect(db, { dbName: "transitDB", useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+// Routes
+
+// @route   GET /api/status/:line
+// @desc    Get status of line (ongoing trip or not?)
+app.get("/api/status/:line", (req, res) => {
+  LineStatus.findById(req.params.line, "ongoing -_id").then(status =>
+    res.send(status)
+  );
+});
+
+// @route   PATCH /api/status/:line
+// @desc    Patch status of line (ongoing trip or not?)
+app.patch("/api/status/:line", (req, res) => {
+  LineStatus.findByIdAndUpdate(req.params.line, req.body).then(status =>
+    res.send(status)
+  );
+});
 
 const port = process.env.PORT || 5000;
 
